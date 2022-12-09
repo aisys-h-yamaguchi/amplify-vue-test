151fac9ddc35d32638f306ad439acd227eef5919
hiroki02 <hiroki.yamaguchi@aisysnet.com>
2022-12-09 16:04:16 +0900

    cognito追加（ローカルでは確認できてない）

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "amplifyvuetestaa7af356": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
